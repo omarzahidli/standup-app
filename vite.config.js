fabb79f3bd00631f1cc9cf1254a0842e1801b85c
omarzahidli <zahidliomer@gmail.com>
2025-11-22 06:28:48 -0800

    Add base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: "/standup-app/",
   plugins: [
     react(),
     tailwindcss()
